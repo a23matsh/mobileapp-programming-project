96c4b88c3ebc063598c7b19123aa05fd1f3f5a4c
a23matsh <a23matsh@student.his.se>
2024-05-06 13:38:38 +0200

    Cleaned out code, now the program runs

--- app/src/main/java/RecyclerViewItem.java
+++ app/src/main/java/RecyclerViewItem.java
@@ -1,2 +1,14 @@
+
+@SuppressWarnings("WeakerAccess")
 public class RecyclerViewItem {
+
+    private String title;
+
+    public RecyclerViewItem(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
