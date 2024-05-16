f7cdea8c91bd6e2a9fbfdf8c7a01e46ee34ad923
a23matsh <a23matsh@student.his.se>
2024-05-16 15:58:56 +0200

    Created a method to fetch JSON data from a URL and updated MainActivity to handle and display a list of Buildings in a RecyclerView using Gson.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,22 +1,28 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
-    private final String JSON_URL = "";
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=a23matsh";
     private final String JSON_FILE = "";
 
-    private ArrayList<RecyclerViewItem> items;
-
-    //private ArrayList<>;
+    private ArrayList<Buildings> buildingsList;
     private RecyclerViewAdapter adapter;
 
     @Override
@@ -26,19 +32,34 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        items = new ArrayList<>(Arrays.asList(
-                new RecyclerViewItem("Matterhorn"),
-                new RecyclerViewItem("Mont Blanc"),
-                new RecyclerViewItem("Denali")
+        buildingsList = new ArrayList<>();
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new RecyclerViewAdapter(this, buildingsList);
+        recyclerView.setAdapter(adapter);
 
+        new JsonTask(this).execute(JSON_URL);
 
 
     }
-
-
 
     @Override
     public void onPostExecute(String json) {
+        Log.d("MainActivity", json);
+        Gson gson = new Gson();
+
+        Type type = new TypeToken<List<Buildings>>() {
+        }.getType();
+        List<Buildings> Buildings = gson.fromJson(json, type);
+
+        adapter.notifyDataSetChanged();
 
     }
-}
+
+        private void getJson() {
+            new JsonTask(this).execute(JSON_URL);
+
+    }
+
+    }
+
